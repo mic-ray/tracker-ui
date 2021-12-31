cdccbb1298c76c264861c0bab16c59a396689845
mic-ray <michael.ray30@gmx.de>
2021-12-31 23:46:16 +0100

    fix: adjust default route to navigate to login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login/Login";
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
